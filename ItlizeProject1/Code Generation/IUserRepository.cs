c7a3658463c756bc4a8bcbc274b5f74ad30fe929
喆鸣 张 <547847623@qq.com>
2019-09-17 17:50:29 -0400

    9/17 17:50

--- ItlizeProject1/Code Generation/IUserRepository.cs
+++ ItlizeProject1/Code Generation/IUserRepository.cs
@@ -3,4 +3,5 @@ using ItlizeProject1.Models;
 public interface IUserRepository : IRepository<User>
 {
     //Add any additional repository methods other than the generic ones (GetAll, GetById, Delete, Add)
+    void InsertNewUser(User user);
 }
